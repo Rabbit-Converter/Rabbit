d28b4bab385c4dc11ad7a4fac6a9b6ade1b0bb65
saturngod <saturngod@gmail.com>
2015-04-18 10:36:00 +0800

    try to clean for gh-pages

--- source/output/python/test.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import Rabbit
-
-text =  u'ယေဓမ္မာ ဟေတုပ္ပဘဝါ တေသံ ဟေတုံ တထာဂတော အာဟ တေသဉ္စ ယောနိရောဓေါ ဧဝံ ဝါဒီ မဟာသမဏော။'
-unicode_text = Rabbit.uni2zg(text)
-zawgyi_text = Rabbit.zg2uni(unicode_text)
-print(unicode_text.encode('utf-8'))
-print(zawgyi_text.encode('utf-8'))
