f007d0cbb6b8279ce45d55bee85dc1c856071006
Saturngod <saturngod@gmail.com>
2015-05-10 11:58:38 +0800

    add build script for node.js module

--- source/build.js
+++ source/build.js
@@ -25,16 +25,22 @@ objc.compile(uni2json,zg2uni,__dirname + "/output/objective-c/Rabbit.m");
 swift.compile(uni2json,zg2uni,__dirname + "/output/swift/Rabbit.swift");
 php.compile(uni2json,zg2uni, version, __dirname + "/output/php/Rabbit.php");
 python.compile(uni2json,zg2uni,__dirname + "/output/python/Rabbit.py");
-ruby.compile(uni2json,zg2uni,__dirname + "/output/ruby/Rabbit.rb");
+ruby.compile(uni2json,zg2uni,__dirname + "/output/ruby/rabbit.rb");
 csharp.compile(uni2json,zg2uni,__dirname + "/output/csharp/Rabbit.cs");
 elixir.compile(uni2json,zg2uni,__dirname + "/output/elixir/Rabbit.ex");
 
 //time to move to Packages
-copy(__dirname + "/output/ruby/Rabbit.rb",path.resolve(__dirname + "/../Packages/ruby/lib/rabbit.rb")); //Ruby
+copy(__dirname + "/output/ruby/rabbit.rb",path.resolve(__dirname + "/../Packages/ruby/lib/rabbit.rb")); //Ruby
 copy(__dirname + "/output/php/Rabbit.php",path.resolve(__dirname + "/../Packages/PHP/src/Rabbit.php")); //PHP
 copy(__dirname + "/output/objective-c/Rabbit.m",path.resolve(__dirname + "/../Packages/Objc/Classes/Rabbit.m")); //objc
 copy(__dirname + "/output/swift/Rabbit.swift",path.resolve(__dirname + "/../Packages/Swift/Classes/Rabbit.swift")); //swift
-copy(__dirname + "/output/javascript/rabbit.js",path.resolve(__dirname + "/../Packages/Node/lib/rabbit.js")); //swift
+
+//for node, need to append string
+copyWithCompletion(__dirname + "/output/javascript/rabbit.js",path.resolve(__dirname + "/../Packages/Node/lib/rabbit.js"),function() {
+	
+	fs.appendFileSync(path.resolve(__dirname + "/../Packages/Node/lib/rabbit.js"), "\nmodule.exports = Rabbit;\n",encoding='utf8');
+	
+});
 
 // Move sample.json to tests folder
 copy(__dirname + "/res/sample.json", path.resolve(__dirname + "/../Packages/PHP/test/unit/sample.json"));
@@ -44,4 +50,13 @@ copy(__dirname + "/res/sample.json", path.resolve(__dirname + "/../Packages/ruby
 
 function copy(from,to) {
   fs.createReadStream(from).pipe(fs.createWriteStream(to));
+}
+
+function copyWithCompletion(from,to,callback) {
+	var stream =fs.createReadStream(from);
+	stream.pipe(fs.createWriteStream(to));
+	
+	stream.on('close', function(){
+		callback();
+	});
 }
--- source/lang/ruby/rabbit_template.rb
+++ source/lang/ruby/rabbit_template.rb
@@ -1,24 +1,30 @@
+require 'rabbit/version'
 require 'json'
 
-# Rabbit Myanmart font converter in Ruby
-class Rabbit
-  def uni2zg(unicode)
-    json_data = '{{UNI2ZG}}'
-    rule = JSON.parse(json_data)
-    return self.replace_with_rule(rule,unicode)
-  end
-
-  def zg2uni(zawgyi)
-    json_data = '{{ZG2UNI}}'
-    rule = JSON.parse(json_data)
-    replace_with_rule(rule,zawgyi)
-  end
-
-  def replace_with_rule(rule, output)
-    rule.each do |data|
-      from = Regexp.new data['from']
-      output = output.gsub(from, data['to'])
+# Rabbit Module
+module Rabbit
+  # Rabbit Convert Class   +  def uni2zg(unicode)
+  # Create an instance as follow  class Converter
+  # Rabbit::Converter.new
+  class Converter
+    def uni2zg(unicode)
+      json_data = '{{UNI2ZG}}'
+      rule = JSON.parse(json_data)
+      return self.replace_with_rule(rule,unicode)
     end
-    output
+
+    def zg2uni(zawgyi)
+      json_data = '{{ZG2UNI}}'
+      rule = JSON.parse(json_data)
+      replace_with_rule(rule,zawgyi)
+    end
+
+    def replace_with_rule(rule, output)
+      rule.each do |data|
+        from = Regexp.new data['from']
+        output = output.gsub(from, data['to'])
+      end
+      output
+    end
   end
 end
