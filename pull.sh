8f722096986cfe4b3817e950881fbd1cf1cbb56c
Saturngod <saturngod@gmail.com>
2016-03-09 11:55:32 +0800

    fixed pull.sh for missing master branch in submodule

--- pull.sh
+++ pull.sh
@@ -3,4 +3,5 @@ git fetch --all
 git pull origin master
 git submodule init
 git submodule update
-git submodule foreach git pull origin master
+git submodule foreach git fetch
+git submodule foreach git checkout master
