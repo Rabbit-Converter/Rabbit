341333dc41988a310b92d29a52bc7c314a764ef9
Saturngod <saturngod@gmail.com>
2017-03-14 09:30:22 +0630

    add new push mehtod for all packages

--- push.sh
+++ push.sh
@@ -2,3 +2,14 @@ git add -A
 git commit -m "$1"
 git push origin master
 
+declare -a arr=("Elixir" "Node" "Objc" "PHP" "ruby" "Swift")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+	cd "$i"
+	git add -A
+	git commit -m "$1"
+	git push origin master
+	cd ..
+done
